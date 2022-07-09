f1efe0d674accaf3a9d3fc75298e0c6ba4d54b74
sabotenpillow <saboten.pillow@gmail.com>
2022-07-10 07:37:45 +0900

    nvim autoload dir

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-if ! [ -d autoload ]; then
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+VIM_AUTOLOAD_DIR=~/.vim/autoload
+if ! [ -d $VIM_AUTOLOAD_DIR ]; then
+  curl -fLo $VIM_AUTOLOAD_DIR/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 else
   echo 'already cloned vim-plug'
@@ -13,14 +14,11 @@ else
   echo 'undo directory is already existed'
 fi
 
-if ! [ -L $HOME/.vimrc ]; then
-  cd && ln -s .vim/.vimrc
+NVIM_AUTOLOAD_DIR="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
+if ! [ -L $NVIM_AUTOLOAD_DIR/plug.vim ]; then
+  mkdir -p $NVIM_AUTOLOAD_DIR \
+  && ln -s $VIM_AUTOLOAD_DIR/plug.vim $NVIM_AUTOLOAD_DIR/plug.vim \
+  && echo "linked '$NVIM_AUTOLOAD_DIR/plug.vim' to '$VIM_AUTOLOAD_DIR/plug.vim' !!!!!"
 else
-  echo 'already created symbolic link to .vimrc'
-fi
-
-if ! [ -L $HOME/.vimshrc ]; then
-  cd && ln -s .vim/.vimshrc
-else
-  echo 'already created symbolic link to .vimshrc'
+  echo "already linked '$NVIM_AUTOLOAD_DIR/plug.vim' to '$VIM_AUTOLOAD_DIR/plug.vim'"
 fi
