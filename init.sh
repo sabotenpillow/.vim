489574795a9dc119b12c7de512b4ec47ffebdb2f
sabotenpillow <saboten.pillow@gmail.com>
2022-07-10 11:28:25 +0900

    nvim data dir link

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-VIM_AUTOLOAD_DIR=~/.vim/autoload
-if ! [ -d $VIM_AUTOLOAD_DIR ]; then
-  curl -fLo $VIM_AUTOLOAD_DIR/plug.vim --create-dirs \
+VIM_DIR=~/.vim
+if ! [ -d $VIM_DIR/autoload ]; then
+  curl -fLo $VIM_DIR/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 else
   echo 'already cloned vim-plug'
@@ -14,11 +14,12 @@ else
   echo 'undo directory is already existed'
 fi
 
-NVIM_AUTOLOAD_DIR="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
-if ! [ -L $NVIM_AUTOLOAD_DIR/plug.vim ]; then
-  mkdir -p $NVIM_AUTOLOAD_DIR \
-  && ln -s $VIM_AUTOLOAD_DIR/plug.vim $NVIM_AUTOLOAD_DIR/plug.vim \
-  && echo "linked '$NVIM_AUTOLOAD_DIR/plug.vim' to '$VIM_AUTOLOAD_DIR/plug.vim' !!!!!"
+## nvim's data dir
+NVIM_DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site
+if ! [ -L $NVIM_DATA_DIR ]; then
+  mkdir -p `dirname $NVIM_DATA_DIR` \
+  && ln -s $VIM_DIR/nvim/data $NVIM_DATA_DIR \
+  && echo "linked '$NVIM_DATA_DIR' to '$VIM_DIR/nvim/data' !!!!!"
 else
-  echo "already linked '$NVIM_AUTOLOAD_DIR/plug.vim' to '$VIM_AUTOLOAD_DIR/plug.vim'"
+  echo "already linked '$NVIM_DATA_DIR' to '$VIM_DIR/nvim/data'."
 fi
